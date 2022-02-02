08a4373dc2e4090a9f580b2e7b5793c2d243eb09
Emin Hayal <eminhayal768@gmail.com>
2022-02-02 16:47:52 +0300

    Initial Commit

---
+++ MyMovie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMovie
+//
+//  Created by Emin Hayal on 2.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMovie/MyMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieApp.swift
+//  MyMovie
+//
+//  Created by Emin Hayal on 2.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
